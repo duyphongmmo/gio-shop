2160a42b0a72834ff3116bb12e8eb3a6cfc8acef
duyphongmmo <duyphongmmo@gmail.com>
2019-08-22 18:00:43 +0700

    ok

---
+++ ASPIdentity/ASPIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPIdentity.Startup))]
+namespace ASPIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
